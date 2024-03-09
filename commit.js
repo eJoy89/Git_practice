d475eec69010cc613c55657b5fb2fd50648a06cd
ejoy89 <cho89@live.co.kr>
2024-03-09 15:32:49 +0900

    branch change

--- commit.js
+++ commit.js
@@ -1,5 +1,5 @@
 console.log('node')
 
-console.log(['a', 'b', 'c'])
+console.log(['a', 'b', 'c', 'd', 'e'])
 
 // 'git log --graph' for seeing git log as graph and to press entre to see previous history
