df7e423def0c506397fb0cfc77f5da6d439ba6a6
ejoy89 <cho89@live.co.kr>
2024-03-17 15:06:51 +0900

    stash

--- FileC.js
+++ FileC.js
@@ -1,2 +1,13 @@
 console.log('Before merge')
 console.log('added something in File C')
+
+
+// PS C:\Users\cho89\OneDrive\바탕 화면\study\Git_practice> git stash apply stash@{2}
+// >>
+// error: unknown switch `e'
+// usage: git stash apply [--index] [-q|--quiet] [<stash>]
+
+//     -q, --quiet           be quiet, only report errors
+//     --index               attempt to recreate the index
+
+// PS C:\Users\cho89\OneDrive\바탕 화면\study\Git_practice> 
