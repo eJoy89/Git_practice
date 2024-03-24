d5387de6f3e6cf055875c2d7f349b0a2a4664308
ejoy89 <cho89@live.co.kr>
2024-03-24 16:00:01 +0900

    receive git

--- FileC.js
+++ FileC.js
@@ -1,24 +1,4 @@
 console.log('Before merge')
 console.log('added something in File C')
 
-<<<<<<< Updated upstream
-
-console.log('commit check')
-
-// PS C:\Users\cho89\OneDrive\바탕 화면\study\Git_practice> git stash apply stash@{2}
-// >>
-// error: unknown switch `e'
-// usage: git stash apply [--index] [-q|--quiet] [<stash>]
-
-//     -q, --quiet           be quiet, only report errors
-//     --index               attempt to recreate the index
-
-
-
-
-console.log('여기 확인')
-
-console.log('before stash')
-=======
 console.log('stash test')
->>>>>>> Stashed changes
