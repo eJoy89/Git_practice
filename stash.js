49c91418606ff8bc3021e94555bc552049fd2133
ejoy89 <cho89@live.co.kr>
2024-04-28 12:12:21 +0900

    stash merge

--- stash.js
+++ stash.js
@@ -1,4 +1,10 @@
 console.log('stash')
 console.log('stash list and stash apply')
 
+<<<<<<< Updated upstream
 console.log('stash list')
+=======
+//
+// 1. git stash로 변경 사항 저장
+// 2. git stash list로 리스트 확인
+>>>>>>> Stashed changes
