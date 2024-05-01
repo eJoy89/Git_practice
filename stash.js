58b1d3da0978d5ffe7490cbdee71e46140f56f39
ejoy89 <cho89@live.co.kr>
2024-05-01 16:38:35 +0900

    first branch commit

---
+++ stash.js
@@ -0,0 +1,12 @@
+console.log('stash')
+console.log('stash list and stash apply')
+
+//
+// 1. git stash로 변경 사항 저장
+// 2. git stash list로 리스트 확인
+
+
+console.log('stash list')
+
+// branch 확인
+console.log('it is first branch')
