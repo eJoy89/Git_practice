16178e8866b908f92b8832ad995fbb9ab8d46167
ejoy89 <cho89@live.co.kr>
2024-05-30 23:11:28 +0900

    temp

--- logCheck.js
+++ logCheck.js
@@ -1,1 +1,24 @@
 // git log -> checkout 
+
+// temp
+
+// document.querySelectorAll('span').forEach(span => span.style.color = 'black');
+// function adjustTrHeight() {
+//     const rows = document.querySelectorAll('tr');
+    
+//     rows.forEach(row => {
+//         const cells = row.querySelectorAll('td');
+//         let maxSpans = 0;
+        
+//         cells.forEach(cell => {
+//             const spanCount = cell.querySelectorAll('span').length;
+//             if (spanCount > maxSpans) {
+//                 maxSpans = spanCount - 1;
+//             }
+//         });
+        
+//         row.style.height = maxSpans * 20 + 'px';
+//     });
+// }
+
+// adjustTrHeight();
