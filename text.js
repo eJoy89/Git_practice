a8205af24a90389ad26d6c86dd800387fdae6c4d
ejoy89 <cho89@live.co.kr>
2024-06-09 11:38:04 +0900

    question

--- text.js
+++ text.js
@@ -27,3 +27,28 @@
 // document.querySelectorAll('td').forEach((list) => {
 //     list.style.height = list.scrollHeight + 'px';
 // });
+
+// 질문
+// function solution(arr) {
+//     let answer = [];
+//     let odds = arr.filter((num) => num % 2 === 1);
+//     let sumOdds = odds.reduce((acc, cur) => (acc += cur), 0);
+//     let minOdd = Math.min(...odds);
+// 	answer.push(sumOdds, minOdd);
+    
+//     let answer = [];
+//     let sum = 0,
+//       min = Number.MAX_SAFE_INTEGER;
+//     for (let x of arr) {
+//        if (x % 2 === 1) {
+//          sum += x;
+//          if (x < min) min = x;
+//        }
+//      }
+//      answer.push(sum);
+//      answer.push(min);
+//      return answer;
+//     }
+
+//    arr = [12, 77, 38, 41, 53, 92, 85];
+//    console.log(solution(arr));
