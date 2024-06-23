abb3ba380ffafba4f94ea9912c9b526bb2749b0e
ejoy89 <cho89@live.co.kr>
2024-06-23 14:38:53 +0900

    cancel

--- stash.js
+++ stash.js
@@ -21,3 +21,9 @@ console.log('something change')
 // How to Merge it?
 // git stash push -m 'message' <- to save with message
 // and when I save through stash, the latest stash will save as stahs@{0} 
+
+// 취소
+// 깃의 reset command to undo a merge.
+// 우선, 커밋 hash (혹은 id)를 확인해야 이전의 커밋으로 돌아갈 수 있습니다.
+// hash를 확인하기 위해서는 git log 혹은 git reflog를 실행하세요. 
+// 더 읽기 쉬운 git reflog가 더 나은 옵션이 될 수 있습니다
